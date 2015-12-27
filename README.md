f27f77ccf532eb38619396226cefd798acaf8a77
root <root@debian-64>
2015-12-27 14:18:40 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pytimezone
 Convert current time with Airport code
+
+root@debian-64:~/# ./timezone.py LHR
+Your location time:2015-12-27 14:16:20.507131
+Europe/London time: 2015-12-27 06:16:20 GMT+0000
+root@debian-64:~/# ./timezone.py hkg
+Your location time:2015-12-27 14:16:25.143637
+Asia/Hong_Kong time: 2015-12-27 14:16:25 HKT+0800
---
+++ timezone.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+import json,re,urllib,sys,os,time
+from sys import argv
+from datetime import datetime
+from pytz import timezone
+
+def checktz(pop):
+    for z in range(0,len(airport)):
+        if pop.upper() in airport[z]["airport_code"]:
+                return airport[z]["tz_code"]
+                
+if len(argv)!=1:
+    pop = argv[1]
+    path=str(sys.path[0])+"/airport.json"
+    json_data=open(path).read()
+    airport = json.loads(json_data)
+    fmt = "%Y-%m-%d %H:%M:%S %Z%z"
+    tz=checktz(pop)
+else:
+    tz=None
+print "Your location time:"+str(datetime.now())
+if (tz==None):
+    print "Sorry , I can't find this POP or Airport"
+else:
+    pop_time = datetime.now(timezone(tz))
+    print tz+" time: "+pop_time.strftime(fmt)
